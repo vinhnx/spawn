8909098e0d5d1a813077b6c83a7735e645bfddca
Vinh Nguyen <vinh.nguyenxuan@2359media.com>
2019-04-17 23:55:07 +0700

    Remove Files package

--- Package.swift
+++ Package.swift
@@ -9,13 +9,12 @@ let package = Package(
         .executable(name: "spawn", targets: ["spawn"])
     ],
     dependencies: [
-        .package(url: "https://github.com/JohnSundell/ShellOut.git", from: "2.0.0"),
-        .package(url: "git@github.com:JohnSundell/Files.git", from: "3.0.0")
+        .package(url: "https://github.com/JohnSundell/ShellOut.git", from: "2.0.0")
     ],
     targets: [
         .target(
             name: "spawn",
-            dependencies: ["ShellOut", "Files"]),
+            dependencies: ["ShellOut"]),
         .testTarget(
             name: "spawnTests",
             dependencies: ["spawn"]),
